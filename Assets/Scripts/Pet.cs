d7e0c54d114062153ce525bf121ea7ff1ac80ed2
Lucy <lucy_g@pop-os.localdomain>
2022-06-08 08:24:41 +0100

    Started implementing first pet

--- Assets/Scripts/Pet.cs
+++ Assets/Scripts/Pet.cs
@@ -1,11 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Pet : MonoBehaviour
 {
     private int happiness;
     private int hunger;
+
+    private int animVal = 0;
+    private int nextAnimVal = 0;
+    private int totalFrames = 0;
+    private itn currentFrame = 1;
+    public List<Sprite> greetingAnim = new List<Sprite>();
+    public List<Sprite> idleAnim = new List<Sprite>();
+    public List<Sprite> walkingAnim = new List<Sprite>();
+    public List<Sprite> feedingAnim = new List<Sprite>();
+
+    public void changeCurrentAnim(int animVal)
+    {
+        
+    }
+
+    public void animCycle()
+    {
+
+    }
+
+    public void start()
+    {
+        changeCurrentAnim(0);
+        animCycle();
+    }
 
     public void feed(int points)
     {
